b2d24559e2b5a8bf478968ef38279a220ae310bf
Paraya <paraya3636@users.noreply.github.com>
2017-03-13 16:58:26 +0900

    Upadte sample

--- app/src/main/java/org/paradrops/materialdatepickerdialogsample/BirthdayPickerDialog.kt
+++ app/src/main/java/org/paradrops/materialdatepickerdialogsample/BirthdayPickerDialog.kt
@@ -1,8 +1,12 @@
 package org.paradrops.materialdatepickerdialogsample
 
 import org.paradrops.materialdatepickerdialog.DatePickerSpinnerDialog
+import org.paradrops.materialdatepickerdialog.Month
 
 class BirthdayPickerDialog : DatePickerSpinnerDialog() {
+    override var defaultYear = 2000
+    override var defaultMonthOfYear = Month.January
+    override var defaultDayOfMonth = 1
     override val title: String = "Your Birthday"
     override val negativeButtonLabel: String = "Cancel"
     override val positiveButtonLabel: String = "OK"
--- app/src/main/java/org/paradrops/materialdatepickerdialogsample/MainActivity.kt
+++ app/src/main/java/org/paradrops/materialdatepickerdialogsample/MainActivity.kt
@@ -3,7 +3,9 @@ package org.paradrops.materialdatepickerdialogsample
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.paradrops.materialdatepickerdialog.DatePickerSpinnerDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +22,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         spinnerDialogButton.setOnClickListener {
-            BirthdayPickerDialog().show(supportFragmentManager)
+            val dialog = BirthdayPickerDialog().apply {
+                onPositiveButtonClickListener = object : DatePickerSpinnerDialog.OnPositiveButtonClickListener {
+                    override fun onClick(dialog: DatePickerSpinnerDialog, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                        Toast.makeText(this@MainActivity, "$year/$monthOfYear/$dayOfMonth", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            dialog.show(supportFragmentManager)
         }
     }
 }
